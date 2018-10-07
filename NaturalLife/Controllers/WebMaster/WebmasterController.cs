584d0ec85bcbfc5fda2b497c27e9f43585ee72b0
Hoang Anh <hoananh27897@gmail.com>
2018-10-08 00:08:02 +0700

    Khoi tao du an

---
+++ NaturalLife/Controllers/WebMaster/WebmasterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NaturalLife.Controllers.WebMaster
+{
+    public class WebmasterController : Controller
+    {
+        // GET: Webmaster
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
